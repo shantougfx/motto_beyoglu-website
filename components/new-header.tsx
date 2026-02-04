737a46002d653d662aad82da6d2b7d26e9b46d47
shantougfx <shantougfx@gmail.com>
2026-02-04 09:18:54 +0300

    Make logo responsive and bigger on all devices

--- components/new-header.tsx
+++ components/new-header.tsx
@@ -116,7 +116,7 @@ export default function NewHeader() {
                                 <img
                                     src="/logo.png"
                                     alt="Motto Beyoğlu"
-                                    className="h-40 w-auto"
+                                    className="h-12 sm:h-16 md:h-20 lg:h-24 w-auto"
                                 />
                             </Link>
 
